bcdad06254c4ff160ee3913b42f890ad85e53833
freedom-developer <freedom_developer@163.com>
2026-07-30 23:11:25 +0800

    update processes_monitor

--- kernel/processes_monitor/processes.c
+++ kernel/processes_monitor/processes.c
@@ -1,4 +1,7 @@
 #include "processes.h"
+#include <linux/seq_file.h>
+#include <linux/sched.h>
+
 
 static struct proc_dir_entry *processes_file = NULL;
 #define PROCESSES_FILENAME "processes"
@@ -27,8 +30,9 @@ static int processes_show(struct seq_file *m, void *data)
 {
     struct task_struct *p;
 
+    seq_printf(m, "%-15s%15s%15s\n", "PID", "PPID", "STATE");
     for_each_process(p) {
-
+        seq_printf(m, "%-15d%-15d%-15c\n", task_pid_nr(p), task_ppid_nr(p), task_state_to_char(p));
     }
 
     return 0;
--- kernel/processes_monitor/ps_monitor.c
+++ kernel/processes_monitor/ps_monitor.c
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
+#include "ps_monitor.h"
 #include "processes.h"
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
